a82e59a629490b8f5e743129705d7a5937795df1
RusselViola <russelviola@gmail.com>
2017-01-02 20:47:44 -0500

    added logic to number count and pressing two buttons at once

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -8,31 +8,39 @@ public class NumberWizard : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
 		max = max + 1;
 		
 		print ("Welcome to Numer Wizard");
 		print ("Pick a number in your head. Just don't tell me..");
-				
+		
 		print("The highest number you can pick is " + max);
 		print("The lowest number you can pick is " + min);
 		
 		print("Is the number higher or lower than " + guess + "?");
 		print("Up = higher, Down = lower, Return = equal");
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			min = guess;
-			guess = (max + min) / 2;
-			print("Higher or lower than " + guess + "?");
+			NextGuess();
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			max = guess;
-			guess = (max + min) / 2;
-			print("Higher or lower than " + guess + "?");
+			NextGuess();
 		} else if (Input.GetKeyDown (KeyCode.Return)) {
 			print ("I WON!");
 		}
 	}
+	
+	void NextGuess () {
+		guess = (max + min) / 2;
+		print("Higher or lower than " + guess + "?");
+		print("Up = higher, Down = lower, Return = equal");
+		
+	}	
 }
